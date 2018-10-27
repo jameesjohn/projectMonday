473ba3a4fc08b3e83082bd1fd81cfc47ccf033c2
mercyikpe <ikpemercy1@bitbucket.org>
2018-10-27 21:54:42 +0100

    registration page redesign

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mastfoot mt-auto">
+  <div class="inner">
+    <p>Designed with <i class="fas fa-heart"></i> by bitsofcodes</p>
+  </div>
+</footer>
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+</button>
+<a class="navbar-brand" href="#"><h3 class="masthead-brand"> Portal</h3></a>
+
+
+<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+</ul>
+<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+<li class="nav-item">
+<a class="nav-link" href="#">Classes</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Forum</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Lectures</a>
+</li>
+</div>
+</nav>
