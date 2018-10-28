a5aaff8e11579c8937287e92fb66da8acdd6571d
ekpono <ekponoambrose@gmail.com>
2018-10-28 21:25:44 +0100

    seeded

--- app/Models/Level.php
+++ app/Models/Level.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Level extends Model
 {
-	public $incrementing = false;
+    public $incrementing = false;
+    
     public function students()
     {
     	$this->hasMany('App\Models\Student');
