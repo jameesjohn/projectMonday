de153ebf4f61c279bea9cf37129450ccf954ec0d
Olatunbosun Egberinde <bosunski@gmail.com>
2018-10-29 08:58:54 +0100

    Added Middleware

--- resources/views/lecturer-home.blade.php
+++ resources/views/lecturer-home.blade.php
@@ -40,12 +40,41 @@
             <li class="nav-item">
                 <a class="nav-link" href="/forum">Lectures</a>
             </li>
+
+
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                <li class="nav-item">
+                    @if (Route::has('register'))
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a> @endif
+                </li>
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                       aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
         </div>
     </nav>
 
     <div role="main" class="cover my-auto">
         <h1 class="cover-heading">Hello</h1>
-        <p class="lead">Welcome to Electrical Electronics Elearning Platform</p>
+        <p class="lead">Welcome to Electrical Electronics E-learning Platform</p>
         <p class="lead">
             <a href="{{ route('new.class') }}" class="btn btn-lg btn-secondary mt-5">Create a Class</a>
             <a href="{{ route('create.assignment') }}" class="btn btn-lg btn-secondary mt-5">Create Assignment</a>
